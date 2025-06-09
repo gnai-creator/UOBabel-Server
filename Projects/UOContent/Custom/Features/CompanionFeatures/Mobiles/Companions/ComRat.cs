34d442b016dc1229c279d9f8395c941f63dc869c
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-06-09 01:06:45 -0300

    fix Comrat

--- Projects/UOContent/Custom/Features/CompanionFeatures/Mobiles/Companions/ComRat.cs
+++ Projects/UOContent/Custom/Features/CompanionFeatures/Mobiles/Companions/ComRat.cs
@@ -30,17 +30,5 @@ namespace Server.Custom.Companions
         public ComRat(Serial serial) : base(serial)
         {
         }
-
-        public override void Serialize(IGenericWriter writer)
-        {
-            base.Serialize(writer);
-            writer.Write(0);
-        }
-
-        public override void Deserialize(IGenericReader reader)
-        {
-            base.Deserialize(reader);
-            _ = reader.ReadInt();
-        }
     }
 }
